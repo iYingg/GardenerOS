07a8750d0937b81e3ad51e5f808fc1634a690d0f
Jintao Zhu <2030442731@qq.com>
2023-12-01 12:03:14 +0800

    实验5

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -1,14 +1,5 @@
 use core::arch::global_asm;
 
-global_asm!(include_str!("trap.S"));
-
-pub fn init() {
-        extern "C" { fn __alltraps(); }
-            unsafe {
-                        stvec::write(__alltraps as usize, TrapMode::Direct);
-                            }
-}
-
 mod context;
 
 use riscv::register::{
@@ -18,11 +9,31 @@ use riscv::register::{
                             self,
                                     Trap,
                                             Exception,
-                                                },
-                                                    stval,
+                                                    Interrupt,
+                                                        },
+                                                            stval,
+                                                                sie,
+};
+use crate::syscall::syscall;
+use crate::task::{
+        exit_current_and_run_next,
+            suspend_current_and_run_next,
 };
 
-use crate::syscall::syscall;
+use crate::timer::set_next_trigger;
+
+global_asm!(include_str!("trap.S"));
+
+pub fn init() {
+        extern "C" { fn __alltraps(); }
+            unsafe {
+                        stvec::write(__alltraps as usize, TrapMode::Direct);
+                            }
+}
+
+pub fn enable_timer_interrupt() {
+        unsafe { sie::set_stimer(); }
+}
 
 #[no_mangle]
 pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
@@ -36,16 +47,20 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
                                     Trap::Exception(Exception::StoreFault) |
                                                 Trap::Exception(Exception::StorePageFault) => {
                                                                 println!("[kernel] PageFault in application, core dumped.");
-                                                                           //run_next_app();
+                                                                            exit_current_and_run_next();
                                                                                     }
                                             Trap::Exception(Exception::IllegalInstruction) => {
                                                             println!("[kernel] IllegalInstruction in application, core dumped.");
-                                                                       // run_next_app();
+                                                                        exit_current_and_run_next();
                                                                                 }
-                                                    _ => {
-                                                                    panic!("Unsupported trap {:?}, stval = {:#x}!", scause.cause(), stval);
-                                                                            }
-                                                        }
+                                                    Trap::Interrupt(Interrupt::SupervisorTimer) => {
+                                                                    set_next_trigger();
+                                                                                suspend_current_and_run_next();
+                                                                                        }
+                                                            _ => {
+                                                                            panic!("Unsupported trap {:?}, stval = {:#x}!", scause.cause(), stval);
+                                                                                    }
+                                                                }
                     cx
 }
 
