f80a553353847f75f77b2070417c11ce12452a58
Jintao Zhu <2030442731@qq.com>
2023-12-01 11:46:06 +0800

    实验4

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,15 +2,17 @@
 #![no_main]
 #![feature(panic_info_message)]
 
+use core::arch::global_asm;
+
 #[macro_use]
 mod console;
 mod lang_items;
 mod sbi;
 mod syscall;
 mod trap;
-mod batch;
-
-use core::arch::global_asm;
+mod loader;
+mod config;
+mod task;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -28,6 +30,7 @@ pub fn rust_main() -> ! {
         clear_bss();
             println!("[Kernel] Hello, world!");
                 trap::init();
-                    batch::init();
-                        batch::run_next_app();
+                    loader::load_apps();
+                        task::run_first_task();
+                            panic!("Unreachable in rust_main!");
 }
